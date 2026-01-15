45f3fc658fd5fba8c951a6d93a80b4ae23e43e1b
xuliangzhan <xu_liangzhan@163.com>
2026-01-15 19:04:16 +0800

    update docs

--- src/plugins/vxe/formDesign/my-custom-form/index.tsx
+++ src/plugins/vxe/formDesign/my-custom-form/index.tsx
@@ -1,6 +1,6 @@
 import { VxeUI } from 'vxe-pc-ui'
 import { MyDesignFormData } from './data'
-import MyDesignFormProps from './MyDesignFormProps.vue'
+import MyDesignSettingForm from './MyDesignSettingForm.vue'
 
 // 创建表单设计器控件 - 自定义表单
 VxeUI.renderer.add('MyDesignForm', {
@@ -16,6 +16,6 @@ VxeUI.renderer.add('MyDesignForm', {
   },
   // 表单设计器 - 渲染控件右侧的表单属性设置
   renderFormDesignSettingFormView (renderOpts, renderParams) {
-    return <MyDesignFormProps render-opts={renderOpts} render-params={renderParams} />
+    return <MyDesignSettingForm render-opts={renderOpts} render-params={renderParams} />
   }
 })
